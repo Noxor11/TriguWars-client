72487bc844d5d8d6ba407e48851b3437cf3952ad
Pablo Alonso <tau0@tutanota.com>
2023-10-15 23:29:54 +0200

    Added text rendering debug hints

--- src/screens/title_screen.cpp
+++ src/screens/title_screen.cpp
@@ -5,6 +5,17 @@
 #include "title_screen.hpp"
 
 void TitleScreen::update() {
+    // Debug
+    graphics::draw_rectangle(vscreen.offset_x, vscreen.offset_y, vscreen.translate_w(vscreen.width), vscreen.translate_h(vscreen.height), {75, 75, 75, 255});
+
+    // Para saber cómo cada librería renderiza el texto
+
+    // Start of text
+    graphics::draw_line(this->vscreen.translate_x(240/2), 0, this->vscreen.translate_x(240/2), this->vscreen.translate_y(320/2), {0, 255, 0, 255});
+
+    // Baseline
+    graphics::draw_line(this->vscreen.translate_x(240/2), this->vscreen.translate_y(320/2), this->vscreen.translate_x(240/2)+30*9, this->vscreen.translate_y(320/2), {0, 0, 255, 255});
+
     graphics::text::draw_text(this->vscreen.translate_x(240/2), this->vscreen.translate_y(320/2),
                         {255,255,255,255}, "TriguWars", 30);
 
@@ -16,10 +27,16 @@ void TitleScreen::init() {
 }
 
 #ifdef __PSVITA__
-#define TITLESCREEN_VSCREEN_SCALE 1.7f
+#define TITLESCREEN_VSCREEN_SCALE 2.26f
 #elif defined __3DS__
 #define TITLESCREEN_VSCREEN_SCALE 1.0f
 #endif
 
-TitleScreen::TitleScreen(): Screen(VirtualScreen(0, 0, 240, 320, TITLESCREEN_VSCREEN_SCALE)) {
+#ifdef __PSVITA__
+#define TITLESCREEN_VSCREEN_OFFSET_X 960 / 2 - (320 * 2.26f) / 2
+#elif defined __3DS__
+#define TITLESCREEN_VSCREEN_OFFSET_X 0
+#endif
+
+TitleScreen::TitleScreen(): Screen(VirtualScreen(TITLESCREEN_VSCREEN_OFFSET_X, 0, 320, 240, TITLESCREEN_VSCREEN_SCALE)) {
 }
