c702c68634aceaa700c36b2ef3ada2356045f7dc
Pablo Alonso <tau0@tutanota.com>
2023-10-20 18:30:45 +0200

    [PolygonObject.draw] more rotation work

--- src/polygonal_object.cpp
+++ src/polygonal_object.cpp
@@ -8,6 +8,11 @@ using graphics::draw_triangle;
 
 void PolygonalObject::draw(const VirtualScreen &vscreen, bool rotate) {
     graphics::Vector2 vertices[this->vertices_count];
+    int screen = 0; // 0 = upper, 1 = lower, 2 = both
+    #ifdef __3DS__
+    float max_y = 0
+    float min_y = 0
+    #endif
     for (int i = 0; i < this->vertices_count; i++) {
       auto v = b2Mul(this->body->GetTransform(), this->vertices[i]);
       // TODO: Escalar en base a vscreen
@@ -15,7 +20,12 @@ void PolygonalObject::draw(const VirtualScreen &vscreen, bool rotate) {
         vertices[i] = {vscreen.translate_x(v.x * vscreen.scale), vscreen.translate_y(v.y * vscreen.scale)};
         std::cout << v.x << "!!" << std::endl;
       } else {
-        vertices[i] = {vscreen.translate_x(v.y * vscreen.scale), vscreen.translate_y(v.x * vscreen.scale)};
+        vertices[i] = {vscreen.translate_x((vscreen.height - v.y) * vscreen.scale), vscreen.translate_y(v.x * vscreen.scale)};
+        #ifdef __3DS__
+        if (i == 0) min_y = vertices[i].y;
+        if (min_y > vertices[i].y) min_y = vertices[i].y;
+        if (max_y < vertices[i].y) max_y = vertices[i].y
+        #endif
       }
     }
     // draw_triangle(this->vertices[0].x, this->vertices[0].y,
