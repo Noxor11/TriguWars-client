2bccc4f9315252b4496deb78f49f6aad31abb4ff
Pablo Alonso <tau0@tutanota.com>
2023-11-06 16:16:38 +0100

    Implement rendering of barrier

--- src/game.cpp
+++ src/game.cpp
@@ -70,13 +70,13 @@ void Game::handle_player_move(){
             }
         }
 
-            if (player_speed > game_config.top_speed || player_speed < -game_config.top_speed) {
-                player_speed = (player_speed / abs(player_speed)) * game_config.top_speed;
-                // Don't
-            } else {
+            //if (player_speed > game_config.top_speed || player_speed < -game_config.top_speed) {
+            //    player_speed = (player_speed / abs(player_speed)) * game_config.top_speed;
+            //    // Don't
+            //} else {
                 b2Vec2 force = b2Vec2(sin(player.body->GetAngle()) * player_speed, -cos(player.body->GetAngle()) * player_speed);
                 player.body->SetLinearVelocity(force);
-            }
+            //}
             //float new_speed = (force + player.body->GetLinearVelocity()).Length();
        // TODO
     } else if (game_config.movement_mode == GameConfig::MovementMode::JOYSTICK2_STRAFE) {
@@ -86,8 +86,6 @@ void Game::handle_player_move(){
             // TODO
         }
     }
-
-
 
     if (game_config.input_compatibility > GameConfig::InputCompatibility::DS) {
         player_rotation_speed += input::joystick1.x * (game_config.rotation_speed / 128.0f);
@@ -100,14 +98,16 @@ void Game::handle_player_move(){
     }
 
 
-    if (player_rotation_speed < game_config.rotation_top_speed && player_rotation_speed > -game_config.rotation_top_speed) {
+    //if (player_rotation_speed < game_config.rotation_top_speed && player_rotation_speed > -game_config.rotation_top_speed) {
         player.body->SetAngularVelocity(player_rotation_speed);
-    } else {
-        player_rotation_speed = (player_rotation_speed / abs(player_rotation_speed)) * game_config.rotation_top_speed;
-    }
+    //} else {
+    //    player_rotation_speed = (player_rotation_speed / abs(player_rotation_speed)) * game_config.rotation_top_speed;
+    //}
 }
 
 void Game::update(float dt) {
+    time_accumulator += dt;
+
     adjust_scale();
     world->Step(dt, velocity_iterations, position_iterations);
 
@@ -158,6 +158,102 @@ void Game::update(float dt) {
         #endif
     }
 
+    if (game_config.enable_barrier) {
+        float color_t = 1.0f;
+        if (game_config.barrier_animation) {
+            color_t = abs(sin(fmod(time_accumulator, game_config.barrier_animation_duration) / game_config.barrier_animation_duration * 3.14f));
+        }
+        graphics::Color color = {(int)round(color_t * 255.0f), 0, 0, 255};
+
+        auto distance = scale * (vscreen.width / 2);
+        if (distance > game_config.barrier_distance || distance < -game_config.barrier_distance) {
+            #ifdef __3DS__
+            float prev_offset = vscreen.offset_x;
+            for(int i = 0; i < 2; i++) {
+                if (i == 1) {
+                    graphics::set_screen(graphics::BOTTOM);
+                    vscreen.offset_x = 0;
+                    vscreen.offset_y = -BOTTOM_SCREEN_HEIGHT;
+                } else {
+                    graphics::set_screen(graphics::TOP1);
+                }
+            // Izquierda
+            graphics::draw_rectangle(
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.height/2) * vscreen.scale + vscreen.offset_x,
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.width/2) * vscreen.scale + vscreen.offset_y,
+                game_config.barrier_distance*2 * scale * vscreen.scale,
+                game_config.barrier_width * scale * vscreen.scale,
+                color
+            );
+            // Arriba
+            graphics::draw_rectangle(
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.height/2) * vscreen.scale + vscreen.offset_x,
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.width/2) * vscreen.scale + vscreen.offset_y,
+                game_config.barrier_width * scale * vscreen.scale,
+                game_config.barrier_distance*2 * scale * vscreen.scale,
+                color
+            );
+            // Derecha
+            graphics::draw_rectangle(
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.height/2) * vscreen.scale + vscreen.offset_x,
+                ((game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.width/2) * vscreen.scale + vscreen.offset_y,
+                game_config.barrier_distance*2 * scale * vscreen.scale,
+                game_config.barrier_width * scale * vscreen.scale,
+                color
+            );
+            // Abajo
+            graphics::draw_rectangle(
+                ((game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.height/2) * vscreen.scale + vscreen.offset_x,
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.width/2) * vscreen.scale + vscreen.offset_y,
+                game_config.barrier_width * scale * vscreen.scale,
+                (game_config.barrier_distance * 2 + game_config.barrier_width) * scale * vscreen.scale,
+                color
+            );
+
+            }
+
+            vscreen.offset_x = prev_offset;
+            vscreen.offset_y = 0;
+
+            graphics::set_screen(graphics::TOP1);
+
+            #else
+            // Izquierda
+            graphics::draw_rectangle(
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.width/2) * vscreen.scale + vscreen.offset_x,
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.height/2) * vscreen.scale + vscreen.offset_y,
+                game_config.barrier_width * scale * vscreen.scale,
+                game_config.barrier_distance*2 * scale * vscreen.scale,
+                color
+            );
+            // Arriba
+            graphics::draw_rectangle(
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.width/2) * vscreen.scale + vscreen.offset_x,
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.height/2) * vscreen.scale + vscreen.offset_y,
+                game_config.barrier_distance*2 * scale * vscreen.scale,
+                game_config.barrier_width * scale * vscreen.scale,
+                color
+            );
+            // Derecha
+            graphics::draw_rectangle(
+                ((game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.width/2) * vscreen.scale + vscreen.offset_x,
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.height/2) * vscreen.scale + vscreen.offset_y,
+                game_config.barrier_width * scale * vscreen.scale,
+                game_config.barrier_distance*2 * scale * vscreen.scale,
+                color
+            );
+            // Abajo
+            graphics::draw_rectangle(
+                ((-game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.width/2) * vscreen.scale + vscreen.offset_x,
+                ((game_config.barrier_distance - game_config.barrier_width) * scale + vscreen.height/2) * vscreen.scale + vscreen.offset_y,
+                (game_config.barrier_distance * 2 + game_config.barrier_width) * scale * vscreen.scale,
+                game_config.barrier_width * scale * vscreen.scale,
+                color
+            );
+            #endif
+        }
+    }
+
     float time_draw = (std::chrono::duration_cast<std::chrono::nanoseconds>)(std::chrono::steady_clock::now() - profdraw).count() / 10E5;
 
     graphics::text::draw_text(10, SCREEN_HEIGHT-120, std::string("logic ").append(std::to_string(time_logic)), 30, false, graphics::Color::BLUE());
@@ -192,24 +288,34 @@ void Game::adjust_scale() {
         }
     }
 
-    float scale_min_x = -(vscreen.width / 2) / min_x;
-    float scale_max_x = (vscreen.width / 2) / max_x;
-    float scale_min_y = -(vscreen.height / 2) / min_y;
-    float scale_max_y = (vscreen.height / 2) / max_y;
-
     float scale_x = 0;
     float scale_y = 0;
-
-    if (scale_min_x < scale_max_x) {
-        scale_x = scale_min_x;
+    auto distance = game_config.barrier_distance * game_config.camera_barrier_percentage;
+    bool abnormal_adjust = false;
+    if (game_config.enable_barrier && (max_x > distance || min_x < -distance || max_y > distance || min_y < -distance)) {
+        scale_x = (vscreen.width / 2) / game_config.barrier_distance * (1.0 - game_config.camera_barrier_zoomout_margin);
+        scale_y = (vscreen.height / 2) / game_config.barrier_distance * (1.0 - game_config.camera_barrier_zoomout_margin);
+        abnormal_adjust = true;
     } else {
-        scale_x = scale_max_x;
-    }
+        float scale_min_x = -(vscreen.width / 2) / min_x;
+        float scale_max_x = (vscreen.width / 2) / max_x;
+        float scale_min_y = -(vscreen.height / 2) / min_y;
+        float scale_max_y = (vscreen.height / 2) / max_y;
 
-    if (scale_min_y < scale_max_y) {
-        scale_y = scale_min_y;
-    } else {
-        scale_y = scale_max_y;
+        scale_x = 0;
+        scale_y = 0;
+
+        if (scale_min_x < scale_max_x) {
+            scale_x = scale_min_x;
+        } else {
+            scale_x = scale_max_x;
+        }
+
+        if (scale_min_y < scale_max_y) {
+            scale_y = scale_min_y;
+        } else {
+            scale_y = scale_max_y;
+        }
     }
 
     if (scale_x < scale_y) {
@@ -217,10 +323,14 @@ void Game::adjust_scale() {
     } else {
         target_scale = scale_y;
     };
+
     if (scale > target_scale) {
         scale_grow_direction = -1.0;
     } else if (scale < target_scale) {
         scale_grow_direction = 1.0;
+        if (!abnormal_adjust) {
+            target_scale *= 0.9;
+        }
     } else {
         return;
     }
